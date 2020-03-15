243ea0934aafd7e92390f66e8015c758ae8a3d41
AmirMr <aomirchik@mail.ru>
2020-03-15 11:36:00 +0600

    empty project first commit

---
+++ src/main/java/kz/ilil/ililapp/IlilAppApplication.java
@@ -0,0 +1,13 @@
+package kz.ilil.ililapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IlilAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IlilAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/ilil/ililapp/IlilAppApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.ilil.ililapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IlilAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
